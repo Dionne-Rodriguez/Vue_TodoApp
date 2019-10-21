14618f698cab04dc8019c0be826cd8c64a96cb24
Dionne Rodriguez <DionneRodriguez.SE@gmail.com>
2019-10-21 10:13:26 -0400

    first commit

---
+++ src/components/Todo.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+    <div v-bind:key= "todo.id" v-for= "todo in todos">
+        <todoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo',todo.id)"/>
+    </div>
+</div>
+</template>
+
+<script>
+
+import todoItem from "./todoItem.vue"
+
+export default {
+    name:'Todos',
+    components:{
+        todoItem
+    }, 
+    props:['todos']
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
